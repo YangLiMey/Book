f5421711d3546bbe64171241be9db5e631255278
Limey Yang <limeyyang@LimeydeMacBook-Air.local>
2017-10-18 13:35:44 +0800

    teatBranch first commit

--- Book/AddViewController.m
+++ Book/AddViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *branchStr = @"git branch commit test";
+    NSLog(@"this is just %@",branchStr);
+    
     // Do any additional setup after loading the view.
 }
 
